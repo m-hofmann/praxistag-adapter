0821107d1b5581579bae08d6b007149ca92cf7b9
Martin Hofmann <m-hofmann@users.noreply.github.com>
2019-01-23 20:28:24 +0100

    Add model for temperature measurement

---
+++ Model/Measurement.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace adapter
+{
+    public class Measurement
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public Double TemperatureCelsius { get; set; }
+    }
+}
