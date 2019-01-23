f34fd76cc31829df57b96172ec879acbb6c78a12
Martin Hofmann <m-hofmann@users.noreply.github.com>
2019-01-23 20:20:28 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
