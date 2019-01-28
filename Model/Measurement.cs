07622337423480181cf3896b96d6e835769d411d
Martin Hofmann <m-hofmann@users.noreply.github.com>
2019-01-28 19:52:05 +0100

    Add device name to measurement property

--- Model/Measurement.cs
+++ Model/Measurement.cs
@@ -1,11 +1,29 @@
 using System;
+using Newtonsoft.Json;
 
 namespace adapter
 {
     public class Measurement
     {
+        /// <summary>
+        /// ID of the device that sent the measurement
+        /// </summary>
+        /// <value>A device name as string</value>
+        [JsonProperty("device_id")]
+        public string DeviceId { get; set; }
+
+        /// <summary>
+        /// Measurement timestamp
+        /// </summary>
+        /// <value>Datetime object</value>
+        [JsonProperty("time_stamp")]
         public DateTime TimeStamp { get; set; }
 
+        /// <summary>
+        /// Temperature measurement in degree celsius
+        /// </summary>
+        /// <value>temperature in °C</value>
+        [JsonProperty("temperature_celsius")]
         public Double TemperatureCelsius { get; set; }
     }
 }
